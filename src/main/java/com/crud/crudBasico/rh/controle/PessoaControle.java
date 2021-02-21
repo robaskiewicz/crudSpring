dcdc0cab6f678040d93e62ebda6ed44fd3641b89
Michel <you@example.com>
2021-02-21 20:14:56 -0300

    inclusao

--- src/main/java/com/crud/crudBasico/rh/controle/PessoaControle.java
+++ src/main/java/com/crud/crudBasico/rh/controle/PessoaControle.java
@@ -3,7 +3,10 @@ package com.crud.crudBasico.rh.controle;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.crud.crudBasico.rh.dominio.Pessoa;
 import com.crud.crudBasico.rh.dominio.PessoaRepositorio;
 
 @Controller
@@ -19,6 +22,17 @@ public class PessoaControle {
 	public String pessoas(Model model) {
 		model.addAttribute("listaPessoas", pessoaRepo.findAll());
 		return "rh/pessoas/index";
+	}
+	
+	@GetMapping("/rh/pessoas/nova")
+	public String novaPessoa(@ModelAttribute("pessoa") Pessoa pessoa) {
+		return "rh/pessoas/form";
+	}
+	
+	@PostMapping("/rh/pessoas/salvar")
+	public String salvarPessoa(@ModelAttribute("pessoa") Pessoa pessoa) {
+		pessoaRepo.save(pessoa);
+		return "redirect:/rh/pessoas";
 	}
 
 }
